51abdc1ec99562a9f5a2b4a6f1c6cec905e15289
mguevara717 <marissa.guevara717@gmail.com>
2018-07-18 22:40:48 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ def long_planeteer_calls(array)
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  cheese = cheese_types.find {|x| array.include?(x)}
+  cheese = cheese_types.select {|x| array.include?(x)}
   return cheese
 end
